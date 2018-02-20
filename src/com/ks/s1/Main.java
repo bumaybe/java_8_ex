ec3e27792eec74b0ee2149277f65a74d3a0c0514
bumaybe <bumaybe@hanmail.net>
2018-02-20 10:23:46 +0900

    CKS
    sysout만 출력하게 바꿈

--- src/com/ks/s1/Main.java
+++ src/com/ks/s1/Main.java
@@ -3,9 +3,10 @@ package com.ks.s1;
 public class Main {
 
 	public static void main(String[] args) {
-		SchoolController s = new SchoolController();
-		s.start();
-
+		System.out.println("Program Start");
+		//SchoolController s = new SchoolController();
+		//s.start();
+		
 	}
 
 }
